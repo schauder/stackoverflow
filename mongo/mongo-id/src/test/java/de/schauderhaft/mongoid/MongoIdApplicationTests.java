090f021793b14c8df43b67b5749f59b09baceea5
Jens Schauder <jschauder@vmware.com>
2021-03-25 08:22:31 +0100

    Mongo Db Testcontainer integration works.

---
+++ mongo/mongo-id/src/test/java/de/schauderhaft/mongoid/MongoIdApplicationTests.java
@@ -0,0 +1,42 @@
+package de.schauderhaft.mongoid;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
+class MongoIdApplicationTests {
+
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.0.10");
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+
+	@Autowired
+	CustomerRepository customers;
+
+	@AfterEach
+	void cleanUp() {
+		customers.deleteAll();
+	}
+	@Test
+	void example() {
+
+		Customer customer = new Customer();
+		customer.name = "Jens";
+		customers.save(customer);
+	}
+
+}
