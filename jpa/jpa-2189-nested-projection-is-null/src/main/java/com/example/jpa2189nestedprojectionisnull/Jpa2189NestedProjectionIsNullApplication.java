2631b3640878dcdf14154ffa16ffc7f7208aae12
Jens Schauder <jschauder@vmware.com>
2021-03-30 07:17:58 +0200

    Created demo project

---
+++ jpa/jpa-2189-nested-projection-is-null/src/main/java/com/example/jpa2189nestedprojectionisnull/Jpa2189NestedProjectionIsNullApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa2189nestedprojectionisnull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa2189NestedProjectionIsNullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpa2189NestedProjectionIsNullApplication.class, args);
+	}
+
+}
---
+++ jpa/jpa-2189-nested-projection-is-null/src/test/java/com/example/jpa2189nestedprojectionisnull/Jpa2189NestedProjectionIsNullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa2189nestedprojectionisnull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa2189NestedProjectionIsNullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
