70fe285ca31a5dd079d2b12411f901d19c8cd07d
Jens Schauder <jschauder@vmware.com>
2021-06-15 09:36:07 +0200

    Sample for JPQL constructor expression.

---
+++ jpa/constructor-expression-with-nested-reference/src/main/java/de/schauderhaft/de/constructorexpressionwithnestedreference/ConstructorExpressionWithNestedReferenceApplication.java
@@ -0,0 +1,13 @@
+package de.schauderhaft.de.constructorexpressionwithnestedreference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructorExpressionWithNestedReferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructorExpressionWithNestedReferenceApplication.class, args);
+	}
+
+}
---
+++ jpa/constructor-expression-with-nested-reference/src/test/java/de/schauderhaft/de/constructorexpressionwithnestedreference/ConstructorExpressionWithNestedReferenceApplicationTests.java
@@ -0,0 +1,48 @@
+package de.schauderhaft.de.constructorexpressionwithnestedreference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static java.util.Arrays.*;
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class ConstructorExpressionWithNestedReferenceApplicationTests {
+
+	@Autowired
+	SomeEntityRepository ents;
+
+	@Test
+	@Transactional
+	void testDtos() {
+
+		createEnts();
+
+		assertThat(ents.findDto()).extracting(Dto::toString).containsExactlyInAnyOrder("ents name - parents name", "parents name - null");
+
+	}
+
+	@Test
+	@Transactional
+	void testEntities() {
+
+		createEnts();
+
+		assertThat(ents.findEntities()).extracting(e -> e.name).containsExactlyInAnyOrder("ents name", "parents name");
+
+	}
+
+	private void createEnts() {
+
+		SomeEntity ent = new SomeEntity();
+		ent.name = "ents name";
+		ent.parent = new SomeEntity();
+		ent.parent.name = "parents name";
+
+
+		ents.saveAll(asList(ent, ent.parent));
+	}
+
+}
